302801b0b9144d69af50f1a45302c9ad10410f40
Triveni G <tg1686222@gmail.com>
2025-07-20 17:24:11 +0530

    Update WebSearcher.java

--- src/app/WebSearcher.java
+++ src/app/WebSearcher.java
@@ -7,12 +7,18 @@ import org.jsoup.select.Elements;
 public class WebSearcher {
     public static String search(String query) {
         try {
-            String url = "https://en.wikipedia.org/wiki/" + query.replace(" ", "_");
-            Document doc = Jsoup.connect(url).get();
-            Elements paragraphs = doc.select("p");
-            return paragraphs.first().text();
+            String url = "https://www.google.com/search?q=" + query.replace(" ", "+");
+            Document doc = Jsoup.connect(url).userAgent("Mozilla/5.0").get();
+            Elements results = doc.select("div[data-attrid='wa:/description'] span");
+            if (!results.isEmpty()) {
+                return results.first().text();
+            } else {
+                return "No result found.";
+            }
         } catch (Exception e) {
-            return "Sorry, I couldn't find information.";
+            e.printStackTrace();
+            return "Failed to fetch result.";
         }
     }
 }
+
